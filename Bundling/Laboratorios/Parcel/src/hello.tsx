ae3ed2766c14941c2af3ba48d994482654aa0129
Eickhel Mendoza <eickhel@hotmail.com>
2018-11-29 22:33:06 +0000

    Bonus 5: Montar lo mismo con Parcel

--- Bundling/Laboratorios/Parcel/src/hello.tsx
+++ Bundling/Laboratorios/Parcel/src/hello.tsx
@@ -2,12 +2,12 @@ import * as React from 'react';
 const img = require('./images/logo.png');
 
 export const HelloComponent = () => (
-  <>
+  <div>
     <div className={'parent'}>
       <div className={'child'}>
-        <p>Hola mundo</p>
+        <p>Hola mundo from Parcel</p>
         <img src={img} alt='Lemoncode logo' />
       </div>
     </div>
-  </>
+  </div>
 );
--- Bundling/Laboratorios/Parcel/src/index.tsx
+++ Bundling/Laboratorios/Parcel/src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { HelloComponent } from './hello';
+import './sass/appStyle.scss';
 
 ReactDOM.render(<HelloComponent />, document.getElementById('root'));
